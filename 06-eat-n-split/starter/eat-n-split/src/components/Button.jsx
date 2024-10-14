332e180dffef0bc630682e4afa686f2a0ebd7ba1
Amit Upreti <amitupreti@carfax.com>
2024-10-13 22:16:55 -0400

    forgot about this

---
+++ 06-eat-n-split/starter/eat-n-split/src/components/Button.jsx
@@ -0,0 +1,9 @@
+export function Button({ children, onClickHandler }) {
+  return (
+    <>
+      <button className="button" onClick={onClickHandler}>
+        {children}
+      </button>
+    </>
+  );
+}
